ef704d842a321c593dc6e67033be3addcf5d3e90
Mouser5 <stanislavegorkin7@gmail.com>
2024-05-11 15:04:13 +0300

    Field with colors

--- main.cpp
+++ main.cpp
@@ -7,22 +7,49 @@ void ChekField() {
 
 }
 
-void FillField(vector <vector <int>>& field, const int size) {
+void FromIntToChar(const int temp,char & color) {
+    if (temp == 1) {
+        color = 'R';
+        return;
+    }
+    if (temp == 2) {
+        color = 'B';
+        return;
+    }
+    if (temp == 3) {
+        color = 'G';
+        return;
+    }
+    if (temp == 4) {
+        color = 'O';
+        return;
+    }
+    if (temp == 5) {
+        color = 'Y';
+        return;
+    }
+    color = 'P';
+    return;
+ }
+
+void FillField(vector <vector <char>>& field, const int size) {
+    int temp = 0;
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            field[i][j] = rand() % 6+1;
+            temp = rand() % 6 + 1;
+            FromIntToChar(temp, field[i][j]);
         }
     }
 }
 
-void CreateField(vector < vector <int>> &field, const int size) {
+void CreateField(vector < vector <char>> &field, const int size) {
     field.resize(size);
     for (int i = 0; i < size; i++) {
         field[i].resize(size);
     }
 }
 
-void PrintField(vector <vector <int>> field,  const int size) {
+void PrintField(vector <vector <char>> field,  const int size) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             cout << field[i][j]<<" ";
@@ -36,7 +63,7 @@ void PrintField(vector <vector <int>> field,  const int size) {
 
 int main()
 {
-    vector <vector <int>> field;
+    vector <vector <char>> field;
     int size = 3;
     CreateField(field, size);
     FillField(field, size);
